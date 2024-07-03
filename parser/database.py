76f67b3c00c2c7550beb0d083a34bacb95a69280
Andrey Torkhov <torkhovasic@yandex.ru>
2024-07-03 14:39:30 +0400

    modified dockefile

--- parser/database.py
+++ parser/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Aat8912000!@localhost:5432/hh"
+DATABASE_URL = "postgresql://postgres:Aat8912000!@my_db:5432/hh"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
